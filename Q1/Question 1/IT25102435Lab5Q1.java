01533de3dac836b6ce12fe75c7057249b59a1ff6
IT25102435 <IT25102435@my.sliit.lk>
2026-09-18 12:30:57 +0530

    Add files via upload

---
+++ Q1/Question 1/IT25102435Lab5Q1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class IT25102435Lab5Q1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = input.nextInt();
+
+        int smallest = num1;
+
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+
+        int largest = num1;
+
+        if (num2 > largest) {
+            largest = num2;
+        }
+
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        System.out.println("Smallest number: " + smallest);
+        System.out.println("Largest number: " + largest);
+    }
+}
